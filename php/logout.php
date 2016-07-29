488c1c032585d4c1c70d128a763239dc776c5f70
pabloHM <hermidamourelle@gmail.com>
2016-07-29 18:35:09 +0200

    Problemas para cerrar sesión

--- php/logout.php
+++ php/logout.php
@@ -3,6 +3,8 @@
 
 	if (isset($_COOKIE['user'])) {
 	    unset($_COOKIE['user']);
+	    unset($_COOKIE['showable']);
 	    setcookie('user', null, -1, '/');
+	    setcookie('showable', null, -1, '/');
 	}
 ?>
